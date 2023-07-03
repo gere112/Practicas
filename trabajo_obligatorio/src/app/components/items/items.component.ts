3c4f20dabba5710080698558c6fd5f429ae62b99
gere112 <gerezellotti2018@gmail.com>
2023-07-03 12:35:10 -0300

    carga de datos

--- trabajo_obligatorio/src/app/components/items/items.component.ts
+++ trabajo_obligatorio/src/app/components/items/items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Item} from "../../models/items";
+import { ItemService } from 'src/app/service/item.service';
 
 @Component({
   selector: 'app-items',
@@ -12,36 +13,14 @@ export class ItemsComponent implements OnInit {
   items: Item[] = [];
   total:number = 0 ;
 
-  constructor(){
+  constructor(private itemService:ItemService){
 
   }
 
 ngOnInit(): void {
   
-    this.items = [
-      {
-        id: 0,
-        title: "manzana",
-        price: 10.5,
-        quantity: 4,
-        completed: false
-      },
-      {
-        id: 1,
-        title: "pan_sintacc",
-        price: 3.5,
-        quantity: 8,
-        completed: true
-      },
-      {
-        id: 2,
-        title: "campera",
-        price: 300,
-        quantity: 1,
-        completed: false
-      }
-    ];
-
+   // this.items = [];
+   this.items = this.itemService.getItems();
     this.getTotal();
 
   }
